b93741fce9736b1b9d68f8d6a3c93893891d4544
SWISSCHEESE <59851390+SWI33@users.noreply.github.com>
2023-10-16 08:55:06 -0500

    Update dockerNetwork.sh

--- dockerNetwork.sh
+++ dockerNetwork.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-# Define network name and subnet
-network_name="WHG3"
-subnet="172.19.0.0/16"
 
-# Define the container name prefix
+network_name="WHG3"
+subnet="10.116.0.0/24"
+
+
 container_name_prefix="mycontainer"
 
-# Remove existing containers
+
 docker rm -f $(docker ps -a --filter "name=$container_name_prefix" --format "{{.ID}}") 2>/dev/null
 
-# Remove the existing network
+
 docker network rm $network_name 2>/dev/null
 
-# Create the Docker network
+
 echo "Creating network $network_name with subnet $subnet"
 docker network create $network_name --subnet=$subnet
 
-# Take user input for the number of containers to create
+
+read -p "Enter the Docker Hub container image (e.g., username/repo:tag): " docker_image
+
+
 read -p "Enter the number of containers to start: " num_containers
 
-# Create the specified number of containers with unique names
+
 for i in $(seq 1 $num_containers); do
   container_name="${container_name_prefix}_${i}"
-  docker run -d --name $container_name --network $network_name 658ee380df1d
+  container_id=$(docker run -d --name $container_name --network $network_name $docker_image)
+  
+
+  container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
+
+  echo "Container ID: $container_id, IP Address: $container_ip"
 done
